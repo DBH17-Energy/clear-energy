474e3829ddc525db4f0839b5104d6b3a66f6c270
DJBnJack <johannes.bertens@gmail.com>
2017-02-09 21:31:01 +0100

    Cleaned up docker-compose, moved util to it's own yml

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ docker pull hyperledger/fabric-baseimage:x86_64-0.2.2
 docker tag hyperledger/fabric-baseimage:x86_64-0.2.2 hyperledger/fabric-baseimage:latest
 
 echo "Downloading go dependencies..."
-docker-compose run utils bash -c 'cd blockchain/src/build-chaincode && GOPATH=$(pwd)/../.. govend -v' && sudo chown -R $USER blockchain
+docker-compose -f docker-util.yml run utils bash -c 'cd blockchain/src/build-chaincode && GOPATH=$(pwd)/../.. govend -v' && sudo chown -R $USER blockchain
 
 echo "Downloading images..."
 docker-compose pull
