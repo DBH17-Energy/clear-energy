7a8be775c33383ed7086f09a02f1bce9ed43de71
Ubuntu <jorisdebeen@Hyperledger.0kdev3kj5ode3db3eex4lcxuzc.ax.internal.cloudapp.net>
2017-02-11 19:57:07 +0000

    overview donuts

--- client/src/app/components/consumption-green/consumption-green.component.ts
+++ client/src/app/components/consumption-green/consumption-green.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consumption-green.component.css']
 })
 export class ConsumptionGreenComponent implements OnInit {
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Green', 'Grey'];
+  public doughnutChartData:number[] = [65, 35];
+  public doughnutChartType:string = 'doughnut';
+  public backgroundColor:any = [{backgroundColor: ["#33dabd", "#9575cd"]}];
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 
   constructor() { }
 
