cb31ac913a65aa7eebec7626c4dd750f0b9335bf
jorisdebeen <github@debeen.com>
2017-02-12 00:21:08 +0000

    Powernow card added. Still have to add linegraph

--- client/src/app/components/power-now/power-now.component.ts
+++ client/src/app/components/power-now/power-now.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PowerNowComponent implements OnInit {
 
+  public powernowChartLabels:string[] = ['Solar', 'Wind', 'hydro'];
+  public powernowChartData:number[] = [55, 40, 5];
+  public powernowChartType:string = 'line';
+  public backgroundColor:any = {backgroundColor: ["#33dabd", "#9575cd"]};
+
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
   constructor() { }
 
   ngOnInit() {
