b4762d1c16d66bd983111232deb53c26aeb2c3e2
jorisdebeen <github@debeen.com>
2017-02-12 00:03:41 +0000

    overview graphs are ming proof

---
+++ client/src/app/components/power-now/power-now.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-power-now',
+  templateUrl: './power-now.component.html',
+  styleUrls: ['./power-now.component.css']
+})
+export class PowerNowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
