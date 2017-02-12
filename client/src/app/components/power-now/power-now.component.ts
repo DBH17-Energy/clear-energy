8d432f184e536170738b6ae6c99809c42df3efb4
jorisdebeen <github@debeen.com>
2017-02-12 09:20:31 +0000

    added angular components

--- client/src/app/components/power-now/power-now.component.ts
+++ client/src/app/components/power-now/power-now.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PowerNowComponent implements OnInit {
 
-  public powernowChartLabels:string[] = ['Solar', 'Wind', 'hydro'];
-  public powernowChartData:number[] = [55, 40, 5];
+  public powernowChartLabels:string[] = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'];
+  public powernowChartData:number[] = [5, 10, 15, 20, 32, 40, 100, 180, 100, 150, 100, 90,120, 60, 90, 520, 580, 700, 1000, 500, 500, 200, 20];
   public powernowChartType:string = 'line';
   public backgroundColor:any = {backgroundColor: ["#33dabd", "#9575cd"]};
 
