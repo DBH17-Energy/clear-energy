b9b8e402ad948a84877e193dbce87c7e59f92cb1
DJBnJack <johannes.bertens@gmail.com>
2017-02-22 22:27:43 +0100

    Fancied the install.bat a bit

--- install.bat
+++ install.bat
@@ -1,14 +1,23 @@
-echo "Getting baseimage..."
+@echo off
+echo Getting baseimage...
+echo.
 docker pull hyperledger/fabric-baseimage:x86_64-0.2.2
 docker tag hyperledger/fabric-baseimage:x86_64-0.2.2 hyperledger/fabric-baseimage:latest
 
-echo "Downloading go dependencies..."
+echo.
+echo Downloading go dependencies...
+echo.
 docker-compose -f docker-util.yml run utils bash -c "cd blockchain/src/build-chaincode && GOPATH=$(pwd)/../.. govend -v"
 
-echo "Downloading images..."
+echo.
+echo Downloading images...
+echo.
 docker-compose pull
 
-echo "Building images..."
+echo.
+echo Building images...
+echo.
 docker-compose build
 
-echo "Done! Start with docker-compose up."
+echo.
+echo Done! Start with docker-compose up.
